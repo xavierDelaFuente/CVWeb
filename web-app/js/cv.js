b26c280876ff7c3ffd89ac5f7922b814f55632e1
xavierDelaFuente <asnewyla@gmail.com>
2017-08-01 15:46:59 +0200

    Add files via upload

---
+++ web-app/js/cv.js
@@ -0,0 +1,135 @@
+//Global data
+CVContainner = [];
+	CVContainner['ProfessionalExperience'] 	= "<div class='container col-md-12'> <table class='table'> <thead> <tr> <th></th> <th>Company Name</th> <th>Location</th> <th>Position</th> <th>Performed on</th> </tr></thead> <tbody> <tr> <td><img src='../img/avanade.jpg'/></td><td>AVANADE </td><td>Barcelona, Spain</td><td>CRM Dynamics Front End Developer (Project Banc Sabadell)</td><td>November 2015 - July 2017</td></tr><tr> <td><img src='../img/learnick.png'/></td><td>LEARNICK</td><td>School: IES Puigvert and Maristes la Immaculada, Barcelona, Spain</td><td>Programing and Robotics instructor</td><td>October 2015 - March 2016 </td></tr><tr> <td><img src='../img/mhh.png'/></td><td>HANNOVER HORZENTRUM (Hearing 4 all)</td><td>Hannover, Germany</td><td>Research and Programming </td><td>August - September 2014</td></tr><tr> <td><img src='../img/femradio.png'/></td><td>FEM RADIO</td><td>Barcelona, Spain</td><td>Sound Technician</td><td>February - May 2014</td></tr></tbody> </table></div>";
+	CVContainner['Experiences'] 			= "<div class='container col-md-12'> <table class='table'> <thead> <tr> <th class='col-md-2'></th> <th class='col-md-2'>Company Name</th> <th class='col-md-3'>Location</th> <th class='col-md-3'>Position</th> <th class='col-md-2'>Performed on</th> </tr></thead> <tbody> <tr> <td><img src='../img/actua.png'/> </td><td>ACTUA </td><td>Andorra la Vella, Andorra</td><td>Google Bussines Photo Project</td><td>Summer 2012</td></tr></tbody> </table></div>";
+	CVContainner['Certifications'] 			= "<div class='container col-md-10'> <table class='table'> <thead> <tr> <th>Studies</th> <th>Location</th> <th>Performed on</th> </tr></thead> <tbody> <tr> <td>Technologic High School </td><td> Sant Ermengol School, Andorra la Vella, Andorra</td><td> - 2011</td></tr><tr> <td>Audiovisual Systems Degree</td><td>Universitat Pompeu Fabra (UPF), Barcelona, Spain</td><td>2012 - 2017</td></tr><tr> <td>Italian: B2 certificate</td><td>Università la Sapienza, Roma, Italy</td><td>2015</td></tr><tr> <td>First Certificate</td><td>Barcelona, Spain</td><td>2013</td></tr><tr> <td>Catalan: C certificate</td><td>Col. Sant Ermengol, Andorra la Vella, Andorra</td><td>2011</td></tr><tr> <td>DELF B1: French Certificate</td><td>Lycée, Andorra la Vella, Andorra</td><td>2010</td></tr></tbody> </table></div>";
+	CVContainner['OtherStudies'] 			= "<div class='container col-md-10'> <table class='table'> <thead> <tr> <th>Studies</th> <th>Location</th> <th>Performed on</th> </tr></thead> <tbody> <tr> <td>Intership</td><td>MHH, Hannover, Germany</td><td>2016</td></tr><tr> <td>Erasmus: Informatics degree</td><td>Università la Sapienza, Roma, Italy</td><td>2014</td></tr></tbody> </table></div>";
+	CVContainner['ProgrammingSkills'] 		= "<div class='container col-md-10'> <h1> Actually i work with </h1> <div class='container col-md-11'> <h2>HTML</h2> <div class='progress'> <div class='progress-bar progress-bar-success' role='progressbar' aria-valuenow='70' aria-valuemin='0' aria-valuemax='100' style='width:90%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Javascript</h2> <div class='progress'> <div class='progress-bar progress-bar-success' role='progressbar' aria-valuenow='95' aria-valuemin='0' aria-valuemax='100' style='width:95%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Jquery</h2> <div class='progress'> <div class='progress-bar progress-bar-success' role='progressbar' aria-valuenow='95' aria-valuemin='0' aria-valuemax='100' style='width:95%'> <span class='sr-only'>70% Complete</span> </div></div><h2>CSS</h2> <div class='progress'> <div class='progress-bar progress-bar-success' role='progressbar' aria-valuenow='70' aria-valuemin='0' aria-valuemax='100' style='width:80%'> <span class='sr-only'>70% Complete</span> </div></div><h2>AJAX</h2> <div class='progress'> <div class='progress-bar progress-bar-success' role='progressbar' aria-valuenow='40' aria-valuemin='0' aria-valuemax='100' style='width:80%'> <span class='sr-only'>70% Complete</span> </div></div><h2>JSON</h2> <div class='progress'> <div class='progress-bar progress-bar-success' role='progressbar' aria-valuenow='40' aria-valuemin='0' aria-valuemax='100' style='width:80%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Microsoft Dynamics CRM</h2> <div class='progress'> <div class='progress-bar progress-bar-success' role='progressbar' aria-valuenow='40' aria-valuemin='0' aria-valuemax='100' style='width:80%'> <span class='sr-only'>70% Complete</span> </div></div></div></div><div class='container col-md-11'> <h1> I have worked with </h1> <div class='container col-md-11'> <h2>MySQL</h2> <div class='progress'> <div class='progress-bar progress-bar-success' role='progressbar' aria-valuenow='70' aria-valuemin='0' aria-valuemax='100' style='width:80%'> <span class='sr-only'>70% Complete</span> </div></div><h2>.NET</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='95' aria-valuemin='0' aria-valuemax='100' style='width:60%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Android</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='95' aria-valuemin='0' aria-valuemax='100' style='width:30%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Matlab</h2> <div class='progress'> <div class='progress-bar progress-bar-success' role='progressbar' aria-valuenow='70' aria-valuemin='0' aria-valuemax='100' style='width:80%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Python</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='40' aria-valuemin='0' aria-valuemax='100' style='width:50%'> <span class='sr-only'>70% Complete</span> </div></div><h2>C #</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='40' aria-valuemin='0' aria-valuemax='100' style='width:40%'> <span class='sr-only'>70% Complete</span> </div></div><h2>JAVA</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='40' aria-valuemin='0' aria-valuemax='100' style='width:40%'> <span class='sr-only'>70% Complete</span> </div></div></div></div><div class='container col-md-11'> <h1> Actually learning </h1> <div class='container col-md-11'> <h2>React</h2> <div class='progress'> <div class='progress-bar progress-bar-warning' role='progressbar' aria-valuenow='95' aria-valuemin='0' aria-valuemax='100' style='width:50%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Angular</h2> <div class='progress'> <div class='progress-bar progress-bar-warning' role='progressbar' aria-valuenow='70' aria-valuemin='0' aria-valuemax='100' style='width:30%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Node</h2> <div class='progress'> <div class='progress-bar progress-bar-warning' role='progressbar' aria-valuenow='95' aria-valuemin='0' aria-valuemax='100' style='width:5%'> <span class='sr-only'>70% Complete</span> </div></div></div></div><div class='container col-md-11'> <h1> Interested in learning </h1> <div class='container col-md-11'> <h2>WebGL</h2> <div class='progress'> <div class='progress-bar progress-bar-danger' role='progressbar' aria-valuenow='70' aria-valuemin='0' aria-valuemax='100' style='width:5%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Unity</h2> <div class='progress'> <div class='progress-bar progress-bar-danger' role='progressbar' aria-valuenow='95' aria-valuemin='0' aria-valuemax='100' style='width:2%'> <span class='sr-only'>70% Complete</span> </div></div></div></div>";	CVContainner['Technicalkills'] 			= "<h1> Technical Skills </h1><div class='container col-md-10'> <h2>CRM Solution Developper</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='70' aria-valuemin='0' aria-valuemax='100' style='width:86%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Front-End Developper</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='95' aria-valuemin='0' aria-valuemax='100' style='width:80%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Machine learning</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='95' aria-valuemin='0' aria-valuemax='100' style='width:70%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Image Processing</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='70' aria-valuemin='0' aria-valuemax='100' style='width:60%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Sound Processing</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='40' aria-valuemin='0' aria-valuemax='100' style='width:55%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Video Processing</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='40' aria-valuemin='0' aria-valuemax='100' style='width:40%'> <span class='sr-only'>70% Complete</span> </div></div></div>";
+	CVContainner['Capacities'] 				= "<h1> Skills </h1><div class='container col-md-10'> <h2>Work in Group</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='70' aria-valuemin='0' aria-valuemax='100' style='width:80%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Organizative capacity</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='95' aria-valuemin='0' aria-valuemax='100' style='width:80%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Adaptability to new environments</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='95' aria-valuemin='0' aria-valuemax='100' style='width:85%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Driving License</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='70' aria-valuemin='0' aria-valuemax='100' style='width:100%'> <span class='sr-only'>70% Complete</span> </div></div></div>";
+	CVContainner['Languajes'] 				= "<div class='container col-md-10'> <h2>English</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='70' aria-valuemin='0' aria-valuemax='100' style='width:80%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Spanish</h2> <div class='progress'> <div class='progress-bar progress-bar-success' role='progressbar' aria-valuenow='95' aria-valuemin='0' aria-valuemax='100' style='width:95%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Catalan</h2> <div class='progress'> <div class='progress-bar  progress-bar-success' role='progressbar' aria-valuenow='95' aria-valuemin='0' aria-valuemax='100' style='width:95%'> <span class='sr-only'>70% Complete</span> </div></div><h2>Italian</h2> <div class='progress'> <div class='progress-bar' role='progressbar' aria-valuenow='70' aria-valuemin='0' aria-valuemax='100' style='width:85%'> <span class='sr-only'>70% Complete</span> </div></div><h2>French</h2> <div class='progress'> <div class='progress-bar progress-bar-warning' role='progressbar' aria-valuenow='40' aria-valuemin='0' aria-valuemax='100' style='width:40%'> <span class='sr-only'>70% Complete</span> </div></div></div>";
+// Functions for collapsing the element on ul
+
+function hideElement(element){
+	element.style.display = 'none';
+} 
+
+function showElement(element){
+	element.style.display = 'block';
+} 
+
+function CollapseDiv (element){
+	if(element.className == 'titulos'){
+		ContainedElements = $(element.parentElement.children[1].children)
+		ContainedElements.each(function(a, b){
+			if(element.getAttribute('value') == '0'){
+				showElement(b);
+				HtmlCollapsed = false;
+				
+			}else{
+				hideElement(b);
+				HtmlCollapsed = true;
+			}
+		})
+		if(HtmlCollapsed == false) element.setAttribute('value', 1);
+			else {
+				element.setAttribute('value', 0);
+			}
+	}
+}
+
+function collapsePersonalInfo(){
+	$("#footerInfo").toggle();
+	$("#CollapsedPersonalInfo").toggle();
+}
+
+function createDIV(element){
+	
+		var htmlElement = '<div class="container col-md-3">'
+				    + '<div style="height: 20%;"> '
+				      + '<img src='+ element.logo +' class="icon">'
+				        + '<h2>'+ element.skill +'</h2>'
+				        + '<div class="progress" style="">'
+				            + '<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:'+ element.progress +'%;"> <span class="sr-only">70% Complete</span> </div>'
+				        + '</div>'
+				   + ' </div>'
+				+ '</div>'		
+	
+	return htmlElement;
+}
+
+//Functions to rediret the page to the corresponding information
+function setContent(element){
+	$('#InfoContainer').empty();
+	switch(element.attributes['name'].value){
+		case 'ProfessionalExperience':
+			$('#InfoContainer').html(CVContainner['ProfessionalExperience'])
+			break;
+		case 'Experiences':
+			$('#InfoContainer').html(CVContainner['Experiences'])
+			break;
+		case 'Certifications':
+			$('#InfoContainer').html(CVContainner['Certifications'])
+			break;
+		case 'OtherStudies':
+			$('#InfoContainer').html(CVContainner['OtherStudies'])
+			break;
+		case 'ProgrammingSkills':
+			$('#InfoContainer').html(CVContainner['ProgrammingSkills'])
+			break;
+		case 'Technicalkills':
+			$('#InfoContainer').html(CVContainner['Technicalkills'])
+			break;
+		case 'Capacities':
+			$('#InfoContainer').html(CVContainner['Capacities'])
+			break;
+		case 'Languajes':
+			$('#InfoContainer').html(CVContainner['Languajes'])
+			break;		
+		case 'FullCV':
+			$('#InfoContainer').html('<div><img src="../img/CV_XaviDeLaFuente_2017.png" /></div>')
+			break;
+		default:
+			console.log('No case');
+			break;
+	};
+}
+
+function getContent(){
+	skills = $.getScript('../Json/skills.json').then(function(){
+		htmlELement = "<br><div class='container col-md-10'>";
+	    htmlELement += "<h1> Actually i work with </h1><br>'";
+	    Currently.forEach(function (a, b){
+			htmlELement += createDIV(a);
+		});
+	    htmlELement += "</div>";
+
+		htmlELement += "<br><div class='container col-md-10'>";
+	    htmlELement += "<h1> I have worked with</h1><br>'";
+	    Worked.forEach(function (a, b){
+			htmlELement += createDIV(a);
+		});
+	    htmlELement += "</div>";
+
+		htmlELement += "<br><div class='container col-md-10'>";
+	    htmlELement += "<h1> Actually learning </h1><br>'";
+	    Learning.forEach(function (a, b){
+			htmlELement += createDIV(a);
+		});
+	    htmlELement += "</div>";
+
+		htmlELement += "<br><div class='container col-md-10'>";
+	    htmlELement += "<h1> Interested in learning </h1><br>'";
+	    Interested.forEach(function (a, b){
+			htmlELement += createDIV(a);
+		});
+
+		htmlELement += "</div>";
+		$('#InfoContainer').html(htmlELement);
+	});
+
+}
+
+//Events on document load
+$(document).ready(function(){
+	$("#CollapsedPersonalInfo").toggle();
+});
